f589cdb5569f3f75527b58a4c23c67a65c98eb51
Violet Peña <violetpena@gmail.com>
2013-01-26 14:06:17 -0500

    Mm, better solution: js resizing on that panel.

--- js/sizing.js
+++ js/sizing.js
@@ -1,6 +1,7 @@
 $(window).on("load resize", function(){
 	height = $(window).height();
+	width = $(window).width();
 	$('#one').height(height);
 	$('#four').height(height);
-	$('#five').height($('.soldiers').height());
+	$('#five').height((775/1000)*width);
 });
